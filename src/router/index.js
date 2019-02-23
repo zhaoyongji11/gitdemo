4d76c7df2cc843768d0b1d3dbdeea59819ea7dbe
zhaoyongji <yongj_0304@163.com>
2019-02-23 10:31:37 +0800

    完成three

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import One from '@/components/one'
 import Two from '@/components/two'
+import Three from '@/components/three'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/two',
       name: 'two',
       component: Two
+    },
+    {
+      path: '/three',
+      name: 'three',
+      component: Three
     }
   ]
 })
